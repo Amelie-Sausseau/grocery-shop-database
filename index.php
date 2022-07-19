83dbafa22c2f91cacf55e19d6f06c9bfa4cf7072
Amélie Sausseau <the_world_is_mine17@hotmail.fr>
2022-07-19 13:55:12 +0200

    navbar + all pages creation

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+session_start();
 require '_header.tpl.php';
 require '_footer.tpl.php';
+?>
+
+<div class="container-xxl">
+    <div class="title">
+        <h1>Bienvenue sur l'épicerie en ligne</h1>
+        <aside>Retrouvez ici tous vos produits d'épicerie livrés directement chez vous !</aside>
+    </div>
+</div>
---
+++ produit.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+require '_header.tpl.php';
+require '_footer.tpl.php';
