e79d3e9277004191c688373e2a49ba52c1a9da8b
Amélie Sausseau <the_world_is_mine17@hotmail.fr>
2022-09-06 11:14:59 +0200

    pages and sql connexion added

--- functions.php
+++ functions.php
@@ -1,4 +1,17 @@
 <?php
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    
+    //On établit la connexion
+    $conn = new mysqli($servername, $username, $password);
+    
+    //On vérifie la connexion
+    if($conn->connect_error){
+        die('Erreur : ' .$conn->connect_error);
+    };
+
 class Article
 {
     public $id;
